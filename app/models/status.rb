7c9e405ab226f58506a45236f4cf972f019940c0
ms2sato <ms2.sato@gmail.com>
2015-12-06 22:40:42 +0900

    statusのロジック追加

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,13 @@
 class Status < ActiveRecord::Base
+
+  # 入室中なら真。さもなくば偽。
+  def inside?
+    logged_out_at.nil?
+  end
+
+  # 滞在時間
+  def length_of_stay
+    end_at = try(:logged_out_at) || Time.current
+    end_at - logged_in_at
+  end
 end
